126b91bb8ff23b5d05fc299a219a89037547e096
Wojciech Jaromin <wojciech.jaromin@euvic.pl>
2021-10-03 15:24:34 +0200

    Initial commit. Added console project for sandbox purposes.

---
+++ WJS.Drive.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WJS.Drive.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
